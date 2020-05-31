25415c35cd0a0d8a993097f689ad1f4f975c2252
John Guerrero <guerrero.johndm@gmail.com>
2020-05-31 21:20:09 +0800

    Updates

---
+++ precache-manifest.e560ad83e6e01865ecf6eda4d840ba10.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780f294b68954ab7e368b5d302d7e2ce",
+    "url": "/johnguerrero/index.html"
+  },
+  {
+    "revision": "45ebb37eb01e4b456d9f",
+    "url": "/johnguerrero/static/css/main.03d4055b.chunk.css"
+  },
+  {
+    "revision": "c992b752d314ea094d1f",
+    "url": "/johnguerrero/static/js/2.5cee117f.chunk.js"
+  },
+  {
+    "revision": "9fa65d6b752e4d283ce3586846e681a8",
+    "url": "/johnguerrero/static/js/2.5cee117f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ebb37eb01e4b456d9f",
+    "url": "/johnguerrero/static/js/main.877857ea.chunk.js"
+  },
+  {
+    "revision": "91631eb9c30aeafe28db",
+    "url": "/johnguerrero/static/js/runtime-main.480f1196.js"
+  },
+  {
+    "revision": "89ccb22cae879a58d5b60b3e3a7baeb6",
+    "url": "/johnguerrero/static/media/Ikaros-Light.89ccb22c.otf"
+  },
+  {
+    "revision": "098c789055e4dcff6df04ac79d1c989f",
+    "url": "/johnguerrero/static/media/Ikaros-Regular.098c7890.otf"
+  },
+  {
+    "revision": "c509123390c78c9d61a9f136d5333eff",
+    "url": "/johnguerrero/static/media/nppl.c5091233.jpg"
+  },
+  {
+    "revision": "1e09402e8c88e6103f18c02c38a32d46",
+    "url": "/johnguerrero/static/media/perpetual-help.1e09402e.jpg"
+  },
+  {
+    "revision": "5820bf538602cf5a6f75418aaec2b69a",
+    "url": "/johnguerrero/static/media/tomoe.5820bf53.jpg"
+  }
+]);
